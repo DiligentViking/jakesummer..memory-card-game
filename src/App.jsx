d405e3bcae2db85b6f97b68584d8a4cd1f1b0ca8
jakesummer <jakersummer@gmail.com>
2026-06-09 13:12:18 -0400

    feat: fetch data from NASA API

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import fetchData from "./fetchData.js";
 
-function App() {}
+function App() {
+  const [apiStatus, setApiStatus] = useState({
+    data: [],
+    isLoading: false,
+    error: false,
+  });
+
+  useEffect(() => {
+    let ignore = false;
+
+    const startFetching = async () => {
+      try {
+        setApiStatus((prev) => ({ ...prev, isLoading: true }));
+        const fetchedData = await fetchData();
+        if (!ignore) {
+          setApiStatus((prev) => ({ ...prev, data: fetchedData }));
+        }
+      } catch (error) {
+        if (!ignore) {
+          setApiStatus((prev) => ({ ...prev, error: error }));
+        }
+      } finally {
+        if (!ignore) {
+          setApiStatus((prev) => ({ ...prev, isLoading: false }));
+        }
+      }
+    };
+
+    startFetching();
+
+    return () => (ignore = true);
+  }, []);
+
+  console.log(apiStatus);
+}
 
 export default App;
